eeff2f52c666846ede4e84d20e8eb0b2cc681937
rodrigotoro <rodrigotoroj@gmail.com>
2021-05-13 11:34:07 +0800

    Complete dna

---
+++ exercises/dna.py
@@ -0,0 +1,24 @@
+import argparse
+
+sample_dataset = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+sample_output = '20 12 17 21'
+
+def count_nucleotides(dna_string):
+    nucleotides = ['A', 'C', 'G', 'T']
+    counts = [(nucleotide, dna_string.count(nucleotide)) for nucleotide in nucleotides]
+
+    print(' '.join([str(count[1]) for count in counts]))
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input')
+    args = parser.parse_args()
+
+    if args.input:
+        with open(args.input, 'r') as f:
+            input_dataset = f.read()
+    else:
+        input_dataset = sample_dataset
+
+    count_nucleotides(input_dataset)

590ac4f641f6680c6b831600ab4dce5218b8d9a9
rodrigotoro <rodrigotoroj@gmail.com>
2021-06-07 23:33:21 +0800

    Refactor using sets for speed optimisation

--- exercises/lcsm.py
+++ exercises/lcsm.py
@@ -11,45 +11,28 @@ ATACA
 
 sample_output = "AC"
 
-TOTAL_CHECKS = 0
-
-
-def get_shortest_fasta(fasta_list):
-    sorted_fasta_list = sorted(fasta_list, key=lambda x: len(x))
-    return sorted_fasta_list[0]
-
-
-def get_all_possible_kmers(fasta, kmer_length):
-    number_of_possible_kmers = len(fasta) + 1 - kmer_length
-    kmers = []
-    for i in range(number_of_possible_kmers):
-        kmers.append(fasta[i : i + kmer_length])
-    return kmers
-
 
 def get_longest_common_substring(multi_fasta_string):
     fastas = [
         Fasta(">" + line) for line in multi_fasta_string.split(">") if len(line) > 0
     ]
 
-    shortest_fasta = get_shortest_fasta(fastas)
+    sorted_fastas = sorted(fastas, key=lambda x: len(x))
 
-    for kmer_length in range(len(shortest_fasta), 0, -1):
-        kmers = get_all_possible_kmers(shortest_fasta, kmer_length)
-        for kmer in kmers:
-            kmer_is_common = []
-            for fasta in fastas:
-                if fasta is not shortest_fasta:
-                    if kmer in fasta.sequence:
-                        kmer_is_common.append(True)
-                    else:
-                        kmer_is_common.append(False)
-                    global TOTAL_CHECKS
-                    TOTAL_CHECKS += 1
-            if all(kmer_is_common):
-                return kmer
+    common_kmers = set()
 
-    return None
+    for i, fasta in enumerate(sorted_fastas):
+        max_kmer_length = None
+        if len(common_kmers) > 0:
+            max_kmer_length = len(max(common_kmers, key=len))
+
+        kmers = fasta.get_kmers(max_length=max_kmer_length)
+        if i == 0:
+            common_kmers = kmers
+        else:
+            common_kmers = common_kmers & kmers
+
+    return max(common_kmers, key=len)
 
 
 if __name__ == "__main__":
@@ -66,4 +49,3 @@ if __name__ == "__main__":
     solution = get_longest_common_substring(input_dataset)
 
     print(solution)
-    print(TOTAL_CHECKS)
